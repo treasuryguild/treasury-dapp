fa4e10ac785c69dbc91b727b7cc7153d6fa00bd5
Andre-Diamond <32074058+Andre-Diamond@users.noreply.github.com>
2023-06-05 06:23:41 +0200

    testing api

--- pages/api/updateContributions.js
+++ pages/api/updateContributions.js
@@ -1,15 +1,9 @@
 import updateTxDatabase from '../../utils/updateTxDatabase'
 
 export default async function handler(req, res) {
-    try {
         const txhash = req.body.record.txhash;
         const myVariable = req.body.record.txinfo;
         const customFilePath = req.body.record.txfilepath;
         const metaData = req.body.record.metadata;
         await updateTxDatabase(myVariable, metaData, txhash, customFilePath)
-
-        res.status(200).json({message: "Success"});
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
 }
