af3818d6907da304b75b9b885f55e23e00af75a2
Andre-Diamond <32074058+Andre-Diamond@users.noreply.github.com>
2023-09-18 15:01:01 +0200

    Updated API routes

--- pages/api/getGWallets.js
+++ pages/api/getGWallets.js
@@ -3,19 +3,24 @@ import axios from 'axios';
 export default async function handler(req, res) {
   const SHEET_ID = process.env.NEXT_PUBLIC_GOOGLE_SHEET;
   const API_KEY = process.env.NEXT_PUBLIC_GOOGLE_API_KEY;
+  const SERVER_API_KEY = process.env.SERVER_API_KEY;
   const API_URL = `https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/'Form responses 1'?key=${API_KEY}`;
+
+  const apiKeyHeader = req.headers['api_key'];
+
+  if (!apiKeyHeader || apiKeyHeader !== SERVER_API_KEY) {
+    return res.status(403).json({ error: 'Forbidden' });
+  }
 
   try {
     const response = await axios.get(API_URL);
     const data = response.data.values;
 
     if (data && data.length > 0) {
-      // Get the index of each column based on the header row
       const timestampIndex = data[0].indexOf("Timestamp");
       const discordHandleIndex = data[0].indexOf("Discord handle");
       const walletAddressIndex = data[0].indexOf("Wallet Address");
 
-      // Map over the rows and return only the necessary columns (ignoring the header row)
       const formattedData = data.slice(1).map(row => ({
         Timestamp: row[timestampIndex],
         DiscordHandle: row[discordHandleIndex],
--- pages/api/getWallets.js
+++ pages/api/getWallets.js
@@ -1,6 +1,13 @@
 import supabase from "../../lib/supabaseClient";
 
-export default async (req, res) => {
+export default async function handler(req, res) {
+  const SERVER_API_KEY = process.env.SERVER_API_KEY;
+  const apiKeyHeader = req.headers['api_key'];
+
+  if (!apiKeyHeader || apiKeyHeader !== SERVER_API_KEY) {
+    return res.status(403).json({ error: 'Forbidden' });
+  }
+
   try {
     let { data, error } = await supabase
       .from('wallets')
