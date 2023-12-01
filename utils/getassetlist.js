078eb67a062af7c7db088bffe1390bc17ae59c2d
Andre-Diamond <32074058+Andre-Diamond@users.noreply.github.com>
2023-12-01 08:27:57 +0200

    Fixed wallet balance nft name bug

--- utils/getassetlist.js
+++ utils/getassetlist.js
@@ -3,8 +3,8 @@ import axios from "axios";
 function isValidKey(key) {
   if (typeof key !== 'string') return false;
   if (key.length > 40) return false;
-  // Start with a letter, not contain any special characters or spaces
-  return /^[A-Za-z][A-Za-z0-9]*$/.test(key);
+  // Start with a letter, can contain letters, numbers, and spaces
+  return /^[A-Za-z][A-Za-z0-9 ]*$/.test(key);
 }
 
 function mapAssetData(assetDetails, assetList) {
