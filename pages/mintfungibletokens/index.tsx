f6b3fc7610625d9bcff96aeeb8d6ca0cdc31384c
André Diamond <32074058+Andre-Diamond@users.noreply.github.com>
2024-09-09 06:57:16 +0200

    Added new minting features

--- pages/mintfungibletokens/index.tsx
+++ pages/mintfungibletokens/index.tsx
@@ -4,9 +4,10 @@ import { Transaction, ForgeScript, resolvePaymentKeyHash, resolveSlotNo } from '
 import type { Mint, AssetMetadata } from '@meshsdk/core';
 import type { NativeScript } from '@meshsdk/core';
 import styles from '../../styles/Minttokens.module.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { getTxAmounts } from "../../utils/gettxamounts";
 import crypto from 'crypto';
+import { getProject } from '../../utils/getProject'
 
 function MintFungibleTokens() {
   const router = useRouter();
@@ -27,6 +28,45 @@ function MintFungibleTokens() {
   const [contributionDetails, setContributionDetails] = useState('');
   const [projectTitle, setProjectTitle] = useState('');
   const [contributorWallet, setContributorWallet] = useState('');
+  const [mintOption, setMintOption] = useState('existing'); 
+  const [allowedTokens, setAllowedTokens] = useState<string[]>([]);
+
+  useEffect(() => {
+    const checkProjectPermissions = async () => {
+      if (connected && wallet) {
+        const addresses = await wallet.getUsedAddresses();
+        const primaryAddress = addresses[0];
+        let projectInfo: any;
+        projectInfo = await getProject(primaryAddress);
+        console.log(projectInfo, projectInfo.project);
+        
+        // Define your logic for which projects can mint which tokens
+        const permissionsMap: { [key: string]: string[] } = {
+          'Singularity Net Ambassador Wallet': ['MINS'],
+          'Test Wallet': ['INF','MINS'],
+          // Add more projects and their allowed tokens as needed
+        };
+
+        setAllowedTokens(permissionsMap[projectInfo.project] || []);
+      }
+    };
+
+    checkProjectPermissions();
+  }, [connected, wallet]);
+
+  const renderExistingTokenButtons = () => {
+    return (
+      <>
+        {allowedTokens.includes('MINS') && (
+          <button className={styles.mint} type="button" onClick={mintMinuteTokens}>Mint Minute Tokens</button>
+        )}
+        {allowedTokens.includes('INF') && (
+          <button className={styles.mint} type="button" onClick={mintINFTokens}>Mint INF Tokens</button>
+        )}
+        {/* Add more token buttons here as needed */}
+      </>
+    );
+  };
 
   const generateHash = (input: any) => {
     return crypto.createHash('sha256').update(input).digest('hex');
@@ -95,6 +135,7 @@ const uploadToIpfs = async () => {
     let imageToUse = tokenData.imageUrl.length != 0 ? tokenData.imageUrl : tokenData.image;
 
     const usedAddress = await wallet.getUsedAddresses();
+    //const changeAddress = await wallet.getChangeAddress();
     const address = usedAddress[0];
     
     const keyHash = resolvePaymentKeyHash(address);
@@ -244,7 +285,23 @@ const uploadToIpfs = async () => {
       website: "https://treasuryguild.io",
       assetQuantity: "1000000",
       image: image,
-      imageUrl: "ipfs://QmSdLfKdaVGL4xgsi5oVMaYLFHx1mAVePvsE63Ncccc3RZ/0" // Replace with actual URL or logic to fetch it
+      imageUrl: "ipfs://QmSdLfKdaVGL4xgsi5oVMaYLFHx1mAVePvsE63Ncccc3RZ/0"
+    };
+    await mintTokens(preTokenData);
+  };
+
+  const mintINFTokens = async () => {
+    // Set your predetermined values
+    const preTokenData = {
+      tokenName: "SingularityNET Ambassador Influance Token",
+      tokenType: "fungible",
+      policy: "open",
+      ticker: "INF",
+      decimals: 0,
+      website: "https://treasuryguild.com",
+      assetQuantity: "10000",
+      image: image,
+      imageUrl: "ipfs://QmbvVUVGxeCREbJ7i79n1ncN152bexBbfsofi2C4P2ZLLF/0"
     };
     await mintTokens(preTokenData);
   };
@@ -269,61 +326,88 @@ const uploadToIpfs = async () => {
     <>
       <div className={styles.body}>
         <form className={styles.form}>
-          <h1>Mint Fungible Tokens</h1>
-          <label className={styles.input}>Policy:</label>
-          <select value={policy} onChange={(e) => setPolicy(e.target.value)}>
-            <option value="closed">Closed</option>
-            <option value="open">Open</option>
+          <h1>Mint Tokens</h1>
+          <label className={styles.input}>Select Minting Option:</label>
+          <select className={styles.selectmint} value={mintOption} onChange={(e) => setMintOption(e.target.value)}>
+            <option value="new">Mint New Token</option>
+            <option value="existing">Mint Existing Token</option>
           </select>
-          <label className={styles.input}>Token Type:</label>
-          <select value={tokenType} onChange={(e) => setTokenType(e.target.value)}>
-            <option value="fungible">Fungible Token</option>
-            <option value="nft">NFT</option>
-          </select>
-          {tokenType == 'nft' && (<>
-            <label className={styles.input}>NFT Type:</label>
-            <select value={nftType} onChange={(e) => setNftType(e.target.value)}>
-              <option value="single">Single user NFT</option>
-              <option value="double">1 user NFT and 1 reference NFT</option>
-            </select>
-            <label className={styles.input}>Project Title:</label>
-            <input type="text" value={projectTitle} onChange={(e) => setProjectTitle(e.target.value)} />
-            <label className={styles.input}>Contribution Details:</label>
-            <input type="text" value={contributionDetails} onChange={(e) => setContributionDetails(e.target.value)} />
-            { nftType == "double" && (<>
-              <label className={styles.input}>Contributor Wallet:</label>
-              <input type="text" value={contributorWallet} onChange={(e) => setContributorWallet(e.target.value)} />
-            </>)}
-          </>)}
-          <label className={styles.input}>Token Name:</label>
-          <input type="text" value={tokenName} onChange={(e) => setTokenName(e.target.value)} />
-          {tokenType == 'fungible' && (<>
-            <label className={styles.input}>Ticker:</label>
-            <input type="text" value={ticker} onChange={(e) => setTicker(e.target.value)} />
-          </>)}
-          {tokenType == 'fungible' && (<>
-            <label className={styles.input}>Decimals:</label>
-            <input type="number" value={decimals} onChange={(e) => setDecimals(parseInt(e.target.value))} />
-          </>)}
-          <label className={styles.input}>Website:</label>
-          <input type="text" value={website} onChange={(e) => setWebsite(e.target.value)} />
-          {tokenType == 'fungible' && (<>
-            <label className={styles.input}>Asset Quantity:</label>
-            <input type="text" value={assetQuantity} onChange={(e) => setAssetQuantity(e.target.value)} />
-          </>)}
-          {/*<label className={styles.input}>Image URL:</label>
-          <input type="text" value={image.join('')} onChange={handleImageChange} /> */}
-            {image.length == 0 && (<>
-              <label className={styles.input}>Image URL: (Enter url or upload picture below)</label>
-              <input type="text" value={imageUrl} onChange={handleImageUrlChange} disabled={image.length > 0} />
-            </>)}
-          {imageUrl.length == 0 && (<div className={styles.imageInput}>
-            <input type="file" onChange={handleFileChange} disabled={imageUrl.length > 0}/>
-            <button type="button" onClick={uploadToIpfs} disabled={imageUrl.length > 0}>Upload</button>
-          </div>)}
-          {file && (<div>Image Uploaded Successfully</div>)}
-          <button className={styles.submit} type="button" onClick={mintNative}>Mint</button>
-          <button className={styles.submit} type="button" onClick={mintMinuteTokens}>Mint Minute tokens</button>
+
+          {mintOption === 'new' && (
+            <>
+              <label className={styles.input}>Policy:</label>
+              <select value={policy} onChange={(e) => setPolicy(e.target.value)}>
+                <option value="closed">Closed</option>
+                <option value="open">Open</option>
+              </select>
+              <label className={styles.input}>Token Type:</label>
+              <select value={tokenType} onChange={(e) => setTokenType(e.target.value)}>
+                <option value="fungible">Fungible Token</option>
+                <option value="nft">NFT</option>
+              </select>
+              {tokenType === 'nft' && (
+                <>
+                  <label className={styles.input}>NFT Type:</label>
+                  <select value={nftType} onChange={(e) => setNftType(e.target.value)}>
+                    <option value="single">Single user NFT</option>
+                    <option value="double">1 user NFT and 1 reference NFT</option>
+                  </select>
+                  <label className={styles.input}>Project Title:</label>
+                  <input type="text" value={projectTitle} onChange={(e) => setProjectTitle(e.target.value)} />
+                  <label className={styles.input}>Contribution Details:</label>
+                  <input type="text" value={contributionDetails} onChange={(e) => setContributionDetails(e.target.value)} />
+                  {nftType === "double" && (
+                    <>
+                      <label className={styles.input}>Contributor Wallet:</label>
+                      <input type="text" value={contributorWallet} onChange={(e) => setContributorWallet(e.target.value)} />
+                    </>
+                  )}
+                </>
+              )}
+              <label className={styles.input}>Token Name:</label>
+              <input type="text" value={tokenName} onChange={(e) => setTokenName(e.target.value)} />
+              {tokenType === 'fungible' && (
+                <>
+                  <label className={styles.input}>Ticker:</label>
+                  <input type="text" value={ticker} onChange={(e) => setTicker(e.target.value)} />
+                  <label className={styles.input}>Decimals:</label>
+                  <input type="number" value={decimals} onChange={(e) => setDecimals(parseInt(e.target.value))} />
+                </>
+              )}
+              <label className={styles.input}>Website:</label>
+              <input type="text" value={website} onChange={(e) => setWebsite(e.target.value)} />
+              {tokenType === 'fungible' && (
+                <>
+                  <label className={styles.input}>Asset Quantity:</label>
+                  <input type="text" value={assetQuantity} onChange={(e) => setAssetQuantity(e.target.value)} />
+                </>
+              )}
+              {image.length === 0 && (
+                <>
+                  <label className={styles.input}>Image URL: (Enter url or upload picture below)</label>
+                  <input type="text" value={imageUrl} onChange={handleImageUrlChange} disabled={image.length > 0} />
+                </>
+              )}
+              {imageUrl.length === 0 && (
+                <div className={styles.imageInput}>
+                  <input type="file" onChange={handleFileChange} disabled={imageUrl.length > 0}/>
+                  <button type="button" onClick={uploadToIpfs} disabled={imageUrl.length > 0}>Upload</button>
+                </div>
+              )}
+              {file && (<div>Image Uploaded Successfully</div>)}
+              <button className={styles.submit} type="button" onClick={mintNative}>Mint</button>
+            </>
+          )}
+
+          {mintOption === 'existing' && (
+            <>
+              {allowedTokens.length > 0 ? (
+                renderExistingTokenButtons()
+              ) : (
+                <p>No tokens available for minting with this wallet address.</p>
+              )}
+            </>
+          )}
         </form>
       </div>
     </>
